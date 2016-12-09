819f248b44febfc820fe41950b8ca9df684d05ca
whywaita <whywaita@whywrite.it>
2016-12-09 23:43:20 +0900

    use yaml config file

--- lab-cleaner-checker.rb
+++ lab-cleaner-checker.rb
@@ -1,43 +1,41 @@
 require 'time'
 require 'slack'
 require 'clockwork'
+require 'yaml'
 
 include Clockwork
 
-count = 0
+config_file = YAML.load_file("config.yml")
+count = config_file["start_num"]
 
 handler do |job|
   case job
   when "lab-cleaner-day.job"
     Slack.configure do |config|
-      config.token = ENV["TOKEN"]
-      if ENV["TOKEN"] == "" then
+      config.token = config_file["slack"]["token"]
+      if config.token == "" then
         p "TOKEN is blank...\n"
         exit(1)
       end
     end
    
-    # 今日の日付を取得
     today = Time.now
-
-    # 班番号
     num_group = [1,2,3,4]
     
     if today.strftime("%w") == "5" then
       # if today Friday
       subject = today.strftime("%x") + "\nToday is Friday!\n" + "Today cleaner group is " + num_group[count].to_s
   
-      # slackへ投稿
       Slack.chat_postMessage(
         channel: '#cleaner-logger',
         username: 'bot',
         text: subject
       )
 
-      if count == 3 then
+      if count == 3 
         count = 0
-      elsif
-        count++
+      else
+        count += 1
       end
 
     end
