8fbf72659a7eba8eadf6012a8167c65590413a67
whywaita <whywaita@whywrite.it>
2016-12-10 18:40:39 +0900

    fix double quote to single quote

--- lab-cleaner-checker.rb
+++ lab-cleaner-checker.rb
@@ -5,14 +5,14 @@ require 'yaml'
 
 include Clockwork
 
-config_file = YAML.load_file("config.yml")
-count = config_file["start_num"]
+config_file = YAML.load_file('config.yml')
+count = config_file['start_num']
 
 handler do |job|
   case job
-  when "lab-cleaner-day.job"
+  when 'lab-cleaner-day.job'
     Slack.configure do |config|
-      config.token = config_file["slack"]["token"]
+      config.token = config_file['slack']['token']
       if config.token == "" then
         p "TOKEN is blank...\n"
         exit(1)
@@ -22,9 +22,9 @@ handler do |job|
     today = Time.now
     num_group = [1,2,3,4]
     
-    if today.strftime("%w") == "5" then
+    if today.strftime('%w') == "5" then
       # if today Friday
-      subject = today.strftime("%x") + "\nToday is Friday!\n" + "Today cleaner group is " + num_group[count].to_s
+      subject = today.strftime('%x') + "\nToday is Friday!\n" + "Today cleaner group is " + num_group[count].to_s
   
       Slack.chat_postMessage(
         channel: '#cleaner-logger',
@@ -40,7 +40,7 @@ handler do |job|
 
     else
       # if today not Friday
-      subject = today.strftime("%x") + "\nToday is not Friday...\n"
+      subject = today.strftime('%x') + "\nToday is not Friday...\n"
 
       Slack.chat_postMessage(
         channel: '#cleaner-logger',
